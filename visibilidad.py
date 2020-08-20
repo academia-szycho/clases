4644193cf41d9526988f4edad955ca638d74fc0e
Gabo de Academia Szycho <licefim254@acceptmail.net>
2020-08-20 20:19:37 -0300

    Nueva versión de visibilidad.py

--- visibilidad.py
+++ visibilidad.py
@@ -1,13 +1,12 @@
-alumnos_noche = ["Gabo", "elwachopiola", "Gise", "Manu Manu", "Mauro19Hz"]
-alumnos_tarde = []
+numero = [5] # Ej: 1028
+multiplicador = 10 #
 
-def agregar_alumno(alumnos, alumno):
-    alumnos = []
-    alumnos.append(alumno)
-    return alumnos
+def multiplicar(a, b):
+    # a = [0] # 1032
+    a[0] = 25
+    return a[0] * b
 
-alumnos_noche = agregar_alumno(alumnos_noche, "mbellesi")
-alumnos_noche = agregar_alumno(alumnos_noche, "Nicky")
-
-print(alumnos_noche)
-print(alumnos_tarde)
+print(multiplicar(numero, multiplicador))
+print(numero[0])
+print(multiplicador)
+print(multiplicar(numero, multiplicador))

b1f53261c5093919893a3522e941adbf1ea280cf
Gabo de Academia Szycho <licefim254@acceptmail.net>
2020-09-10 20:28:49 -0300

    Subo 6_5_b

--- ejercicio_6_5_b.py
+++ ejercicio_6_5_b.py
@@ -4,20 +4,24 @@ b) Dicha cadena con la primera letra de cada palabra en mayúsculas. Por ejemplo
 'república argentina' debe devolver 'República Argentina'.
 '''
 
-def titular(cadena):
+def mayuscula(cadena):
     '''
-    Devuelve la primera letra de cada palabra en mayúsculas. Por ejemplo, si recibe
-    'república argentina' debe devolver 'República Argentina'.
-
+    Devuelve la primera letra de cada palabra.
             Parameters:
                     cadena (string): una cadena de texto
-
             Returns:
-                    (str): una cadena de texto con la primer letra de cada palabra en mayúscula
-    '''
-    return cadena.title()
+                    (str): una cadena con la primera letra de cada palabra en mayúsculas
+'''
 
-lista = titular("repúBlica argentina")
+    lista_palabras = cadena.split(" ")
+    nueva_frase = []
+    for palabra in lista_palabras:
+        primera_letra = palabra[0]
+        nueva_palabra = primera_letra.upper() + palabra[1:]
+        nueva_frase.append(nueva_palabra)
+    return " ".join(nueva_frase)
+
+lista = mayuscula("república argentina")
 print(lista)
 
 
